a861707db3fcace105b862b8a504de3c90edd239
neac-fuel <ehalcyon@gmail.com>
2025-07-04 13:25:45 -0600

    Create hunt.js

---
+++ include/hunt.js
@@ -0,0 +1,14 @@
+var params = new URLSearchParams(document.location.search);
+var query = params.get("search");
+
+if (query) {
+	var sanitizedQuery = query.trim().replace(/[\s:]+/g, '-').toLowerCase();
+	
+	var showClue = document.getElementById(sanitizedQuery);
+	if (!showClue) {
+		document.getElementById('search-term').textContent = query;
+		showClue = document.getElementById('no-result');
+	}
+	showClue.style.display = "block";
+	
+}
